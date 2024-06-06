a039c60ed869ee1688ae1535e348a11a2b5a8c2a
unknown <atzin_tona@hotmail.com>
2024-06-06 17:04:53 -0600

    nuevo repo

---
+++ script.js
@@ -0,0 +1,73 @@
+let mensaje=localStorage.getItem("mensajeLocal")
+
+console.log(mensaje)
+
+let productos = [
+    {
+        id: "1",
+        name: "cocina",
+        price: 12000,
+        Img: "http://picsum.photos/200/300",
+        description: "cocina de herreria",
+    },
+    {
+        id: "2",
+        name: "mesa",
+        price: 2500,
+        Img: "http://picsum.photos/200/300",
+        descripcion:"mesa de herreria",
+    },
+    {
+        id: "3",
+        name: "mueble",
+        price: 5000,
+        Img: "http://picsum.photos/200/300",
+        description: "mueble de herreria con madera",
+    },
+    {
+        id: "4",
+        name: "repisa",
+        price: 2500,
+        Img: "http://picsum.photos/200/300",
+        descripcion:"repisa de herreria",
+    },
+    
+    {
+        id: "5",
+        name: "repisa rombo",
+        price: 2000,
+        Img: "http://picsum.photos/200/300",
+        descripcion:"perisa rombo de herreria con madera",
+    },
+
+
+
+
+];
+
+function mostrarProductos(){
+  let contenedor = document.querySelector("#productos");
+
+  for (const product of productos ) {
+    contenedor.innerHTML += 
+    `
+    <div class="card" id=${product.id}>
+      <img src=${product.Img} alt=${product.description}>
+      <div class="card-produtos">
+        <h3>${product.name}</h3>
+        <p>${product.description}</p>
+        <b>${product.price}</b>
+        <button-type class="agregar-carrito" data-id=${product.id}>Agregar</button-type>
+      </div>
+    </div>
+     `
+  } 
+  document.querySelectorAll(".agregar-carrito").forEach(btn {
+    btn.addEventlisterent("click",() {
+      const productID = getAttribute( "data-id");
+      console.log(productID)
+    })
+  })
+}
+
+mostrarProductos()
